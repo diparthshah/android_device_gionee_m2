43e22c132c4e31aa8d565081cee9054ae99d7ce2
DIPARTH SHAH <diparths@gmail.com>
2016-06-02 14:55:27 +0530

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -3,6 +3,10 @@ PRODUCT_RELEASE_NAME := m2
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit Suitabe GPS Code
+$(call inherit-product, device/common/gps/gps_us_supl.mk)
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
