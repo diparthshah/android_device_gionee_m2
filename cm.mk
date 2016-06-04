6d64a2970c6de258149cdd690e0b104b629e0705
DIPARTH SHAH <diparths@gmail.com>
2016-06-04 14:37:59 +0530

    Add and Update

--- cm.mk
+++ cm.mk
@@ -1,9 +1,8 @@
 # Release name
 PRODUCT_RELEASE_NAME := m2
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+# Inherit Complete Device Configuration
+$(call inherit-product, device/gionee/m2/full_m2.mk)
 
 # Inherit Suitabe GPS Code
 $(call inherit-product, device/common/gps/gps_us_supl.mk)
@@ -11,17 +10,12 @@ $(call inherit-product, device/common/gps/gps_us_supl.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-# Inherit device configuration
-$(call inherit-product, device/gionee/m2/device_m2.mk)
-
-# Inherit Proprietary Stuff
-$(call inherit-product, vendor/gionee/m2/m2-vendor.mk)
-
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := m2
 PRODUCT_NAME := cm_m2
 PRODUCT_BRAND := gionee
 PRODUCT_MODEL := m2
 PRODUCT_MANUFACTURER := gionee 
+
 
 
